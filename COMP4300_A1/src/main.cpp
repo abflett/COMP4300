21d979ea2c4a56f32bf5985f089b8b02e9abfa1c
Adam Flett <adam@adamflett.net>
2024-02-07 19:01:57 +0800

    fixed configuration and usable font

--- COMP4300_A1/src/main.cpp
+++ COMP4300_A1/src/main.cpp
@@ -4,6 +4,36 @@
 
 int main(int argc, char* argv[])
 {
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 
-	return 0;
+	// Create a graphical text to display
+	sf::Font font;
+	if (!font.loadFromFile("fonts/tech.ttf"))
+		return EXIT_FAILURE;
+	sf::Text text("Hello SFML", font, 50);
+
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		// Clear screen
+		window.clear();
+
+		// Draw the string
+		window.draw(text);
+
+		// Update the window
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
 }
